b7b958f94e775e3516425c50c232b85deb72a5e2
Kerllos <kerllos.samir@gmail.com>
2019-12-24 11:30:30 -0600

    Adding Check Master Code method

--- Program.cs
+++ Program.cs
@@ -11,7 +11,44 @@ namespace Mastermind
         static void Main(string[] args)
         {
             int[] code = CodeGenerator.Generate();
+            int attempts = 0;
+            while (attempts < 10)
+            {
+                Console.WriteLine("Please enter 4 digits (1 to 6) code");
+                string userCode = Console.ReadLine();
+                int intCode = 0;
 
+                if (int.TryParse(userCode, out intCode))
+                {
+
+                    if (userCode.Length != 4)
+                    {
+                        Console.Write("Code length not valid ,");
+                    }
+                    else
+                    {
+                        //Console.WriteLine(userCode);
+                        string res;
+                        int[] userCodeDigits = Utility.ExtractUserInputDigits(userCode);
+                        if (Utility.CheckMasterCode(code, userCodeDigits, out res))
+                        {
+                            Console.WriteLine("Congratulations you won");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry you didn't win , kindly try again " + res);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.Write("Invalid number ,");
+                }
+
+                attempts++;
+            }
+            Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
     }
---
+++ Utility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    class Utility
+    {
+        protected internal static bool CheckMasterCode(int[] masterCode, int[] userCode, out string result)
+        {
+            StringBuilder compination = new StringBuilder();
+            bool match = true;
+            for (int i = 0; i < 4; i++)
+            {
+                if (masterCode[i] == userCode[i])
+                {
+                    compination.Append("+");
+                }
+                else if (masterCode.Contains(userCode[i]))
+                {
+                    compination.Append("-");
+                    match = false;
+                }
+                else
+                {
+                    compination.Append(" ");
+                    match = false;
+                }
+            }
+            result = compination.ToString();
+
+            return match;
+        }
+
+        protected internal static int[] ExtractUserInputDigits(string userInput)
+        {
+            int[] userCode = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                userCode[i] = int.Parse(userInput[i].ToString());
+            }
+            return userCode;
+        }
+    }
+}
